04032be49d225e35e451c18ffa3ffab1f3b635b3
zzooong2 <zzooong2@naver.com>
2024-01-23 16:39:10 +0900

    Player List 페이지 구현

--- frontend/src/components/posts/ListForm.vue
+++ frontend/src/components/posts/ListForm.vue
@@ -1,35 +1,29 @@
 <template>
-  <button @click="showPlayerList"> Show Player List </button>
-  <p>{{ data }}</p>
-<!--  <v-table>-->
-<!--    <thead>-->
-<!--      <tr>-->
-<!--        <th>name</th>-->
-<!--        <th>position</th>-->
-<!--        <th>backnumber</th>-->
-<!--      </tr>-->
-<!--    </thead>-->
-<!--    <tbody>-->
-<!--      <tr v-for="item in list" :key="item">-->
-<!--        <td>{{ item.name }}</td>-->
-<!--        <td>{{ item.position }}</td>-->
-<!--        <td>{{ item.backnumber }}</td>-->
-<!--      </tr>-->
-<!--    </tbody>-->
-<!--  </v-table>-->
+  <router-link to="/"> MainPage </router-link>
+  <br>
+  <div v-for="(item, idx) in list" :key="idx">
+    <p>name: {{ item.name }}</p>
+    <p>position: {{ item.position }}</p>
+    <p>backnumber: {{ item.backnumber }}</p>
+  </div>
 </template>
 
 <script>
-import {showPlayerList} from "@/api";
-
+import { showPlayerList } from '@/api/index.js'
 export default {
   data() {
     return {
-      data: [],
+      list: {
+        name: '',
+        position: '',
+        backnumber: '',
+      },
     };
   },
-  methods: {
-    showPlayerList,
+  created() {
+    showPlayerList()
+        .then(response => (this.list = response.data.list))
+        .catch(error => console.log(error));
   }
 }
 </script>
