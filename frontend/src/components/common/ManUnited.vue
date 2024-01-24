2f951d43aa16bc397105267828cfa40dc92219b9
zzooong2 <zzooong2@naver.com>
2024-01-24 14:35:36 +0900

    api데이터 바인딩

--- frontend/src/components/common/ManUnited.vue
+++ frontend/src/components/common/ManUnited.vue
@@ -11,6 +11,8 @@ export default {
 }
 </script>
 
+<!--router-link를 html로 보면 <a> 태그로 사용된다.-->
+<!--a에 대한 css 를 지정하여 ListPage의 밑줄 제거-->
 <style>
 h1 {
   text-align: center;
@@ -19,6 +21,9 @@ img{
   margin: auto;
   display: block;
   width: 60%;
+}
+a {
+  text-decoration: none;
 }
 
 </style>
--- frontend/src/components/posts/ListForm.vue
+++ frontend/src/components/posts/ListForm.vue
@@ -1,15 +1,27 @@
 <template>
   <router-link to="/"> MainPage </router-link>
   <br>
-  <div v-for="(item, idx) in list" :key="idx">
-    <p>name: {{ item.name }}</p>
-    <p>position: {{ item.position }}</p>
-    <p>backnumber: {{ item.backnumber }}</p>
-  </div>
+  <table style="margin-left: auto; margin-right: auto;">
+    <thead>
+      <tr>
+        <th>선수명</th>
+        <th>포지션</th>
+        <th>등번호</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in list" :key="item.backnumber">
+        <td>{{item.name}}</td>
+        <td>{{item.position}}</td>
+        <td>{{item.backnumber}}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 import { showPlayerList } from '@/api/index.js'
+
 export default {
   data() {
     return {
@@ -17,17 +29,25 @@ export default {
         name: '',
         position: '',
         backnumber: '',
-      },
-    };
+      }
+    }
   },
   created() {
+    var vm = this;
     showPlayerList()
-        .then(response => (this.list = response.data.list))
-        .catch(error => console.log(error));
-  }
+        .then(function (response){
+          console.log(response);
+          vm.list = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+  },
 }
 </script>
 
 <style>
-
+table {
+  text-align: center;
+}
 </style>
